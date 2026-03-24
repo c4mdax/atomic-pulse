d33c0650d3083b1a00fc5805c33627dc85360b57
Luis Angel <c4mdax@ciencias.unam.mx>
2026-03-23 23:49:03 -0600

    Adding fetch_nuclear_outages method

--- src/connector.py
+++ src/connector.py
@@ -21,3 +21,31 @@ class EIAConnector:
         if not self.api_key:
             raise ValueError("Missing API Key. Please check the .env file.")
 
+    def fetch_nuclear_outages(self):
+        """
+        Queries the EIA API for the latest nuclear plant outage data.
+        Returns:
+           pd.DataFrame: Cleaned DataFrame containing outage events.
+           or None if the request failed.
+        """
+        params = {
+            "api_key":self.api_key,
+            "data[]":["outage_capacity", "capacity"],
+            "frequency":"daily",
+            "sort[0][column]":"period",
+            "sort[0][direction]":"desc",
+            "offset":0,
+            "length":5000
+        }
+
+        try:
+            print("Connecting to EIA Open Data API...")
+            response = requests.get(self.base_url, params=params)
+            response.raise_for_status()
+            json_data = response.json()
+            raw_data = json_data['response']['data']
+            return pd.DataFrame(raw_data)
+
+        except Exception as e:
+            print(f"Connection error: {e}")
+            return None
