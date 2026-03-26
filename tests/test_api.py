b01c4d543ce4b8f59bf441db9b08b2ecbe3aba64
Luis Angel <c4mdax@ciencias.unam.mx>
2026-03-26 16:48:42 -0600

    Adding test for api: root response

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,9 +2,18 @@ import pytest
 from fastapi.testclient import TestClient
 from src.api import app
 
+"""
+Purpose: Follow TDD Standards
+"""
 cliente = TestClient(app)
 
-def test_read_root()
+def test_read_root():
+    """
+    Test if the root's response is ok
+    """
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"status" : "Nuclear Outages API is Online", "version: 1.0.0"}
 
 def test_get_outages_format()
 
