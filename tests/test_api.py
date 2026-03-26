3a900fdc5f520da8efe9ce832387a06a91eb5545
Luis Angel <c4mdax@ciencias.unam.mx>
2026-03-26 16:46:17 -0600

    Adding tests for api

---
+++ tests/test_api.py
@@ -0,0 +1,11 @@
+import pytest
+from fastapi.testclient import TestClient
+from src.api import app
+
+cliente = TestClient(app)
+
+def test_read_root()
+
+def test_get_outages_format()
+
+def test_get_summary()
