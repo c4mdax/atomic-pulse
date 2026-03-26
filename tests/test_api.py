45854e809d1f22576018b953dfdb856b3f7ab00f
Luis Angel <c4mdax@ciencias.unam.mx>
2026-03-26 17:20:08 -0600

    Finished tests for api

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,7 +5,7 @@ from src.api import app
 """
 Purpose: Follow TDD Standards
 """
-cliente = TestClient(app)
+client = TestClient(app)
 
 def test_read_root():
     """
@@ -13,8 +13,27 @@ def test_read_root():
     """
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"status" : "Nuclear Outages API is Online", "version: 1.0.0"}
+    assert response.json() == {"status" : "¡Nuclear Outages API is Online!", "version": "1.0.0"}
 
-def test_get_outages_format()
+def test_get_outages_format():
+    """
+    Test if the outages endpoint returns a list with correct parameters
+    """
+    response = client.get("/outages?limit=1")
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
+    if len(data) > 0:
+        record = data[0]
+        expected_keys = {"id", "date_key", "status_id", "capacity_mw", "outage_mw", "percent_outage"}
+        assert expected_keys.issubset(record.keys())
 
-def test_get_summary()
+def test_get_summary():
+    """
+    Test the analytic endpoint
+    """
+    response = client.get("/outages/summary")
+    assert response.status_code == 200
+    data = response.json()
+    assert "total_records" in data
+    assert "avg_outage_mw" in data
